6a7de22b70824d41e4b1c110ae43c697d8a3e4c1
Mwithiga-lab <128584553+Mwithiga-lab@users.noreply.github.com>
2025-01-23 22:25:42 +0300

    Add files via upload

---
+++ helloWorld.java
@@ -0,0 +1,16 @@
+/*
+Program to print Hello, World!
+Author: Kevin Mwithiga
+Reg: CT101/G/19870/23
+Date: 22/1/2025
+*/
+//helloWorld class
+public class helloWorld
+{
+// Main Method
+public static void main(String[] args)
+{
+//Print statement
+System.out.println("Hello, World!");
+}
+}
